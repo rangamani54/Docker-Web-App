0e169adab13c9a057528b9e6c9a13deb8447e80c
mani kumar <ranga.mani54@gmail.com>
2021-06-27 02:17:19 +0530

    Added file

---
+++ mainapp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+import cgi
+import subprocess as sp
+
+print("content-type: text/html")
+print()
+
+f = cgi.FieldStorage()
+cmd = f.getvalue("x")
+akey = sp.getstatusoutput("sudo " + cmd)
+#status = akey[0]
+#output = akey[1]
+if akey[0] == 0:
+    print(akey[1])
+else:
+    print("Error Occured: {} ".format(akey[1]))
+
